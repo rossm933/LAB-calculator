9728a4a5d4e8fcb03cdf7a561f5bb8aee2a84aec
Ross Morgan <ross.morgan933@gmail.com>
2024-01-23 08:24:30 -0600

    Added js to buttons

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("I am here!");
+
+function appendToResult(value) {
+  let display = document.getElementById('display');
+  if(display.textContent === "0") {
+    display.textContent = value;
+  } else {
+    display.textContent += value;
+  }
+}
+function calculateResult() {
+  let display = document.getElementById('display');
+  let expression = display.textContent;
+  let answer = eval(expression);
+  display.textContent = answer;
+}
+
+function deleteLastCharacter() {
+display.innerText = display.innerText.slice(0, -1);
+ }
+
+
+function clearResult() {
+ document.getElementById('display').innerHTML = "";
+}
